182a3c9515422172a1b4ada7b537f20a5c2b50bd
Daan van der Kallen <mail@daanvdk.com>
2020-01-20 12:05:27 +0100

    Fix TimeInput

--- src/TimeInput.js
+++ src/TimeInput.js
@@ -1,66 +1,8 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Input } from 'semantic-ui-react';
-import { DateTime } from 'luxon';
+import DateInput from './DateInput';
 
-export default class TimeInput extends Component {
-    static propTypes = {
-        format: PropTypes.string,
-        value: PropTypes.instanceOf(DateTime),
-        onChange: PropTypes.func.isRequired,
-    };
-
+export default class TimeInput extends DateInput {  
     static defaultProps = {
+        ...DateInput.defaultProps,
         format: 'H:mm',
     };
-
-    state = { value: null };
-
-    constructor(...args) {
-        super(...args);
-        this.onChange = this.onChange.bind(this);
-        this.onBlur = this.onBlur.bind(this);
-    }
-
-    onChange(e, { value }) {
-        const { format, onChange } = this.props;
-
-        this.setState({ typeValue: value });
-
-        const date = DateTime.fromFormat(value, format);
-        if (!date.invalid) {
-            onChange(date);
-        }
-    }
-
-    onBlur(...args) {
-        const { onBlur } = this.props;
-        this.setState({ typeValue: null });
-        if (onBlur) {
-            onBlur(...args);
-        }
-    }
-
-    render() {
-        const { typeValue } = this.state; 
-        const { format, value, ...props } = this.props;
-
-        delete props.onChange;
-        delete props.onBlur;
-
-        return (
-            <Input
-                value={
-                    typeValue !== null
-                    ? typeValue
-                    : value
-                    ? value.toFormat(format)
-                    : ''
-                }
-                onChange={this.onChange}
-                onBlur={this.onBlur}
-                {...props}
-            />
-        );
-    }
 }
